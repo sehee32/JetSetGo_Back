9d9f65038b59c17a2f116969154c1afd73bab828
문유정 <adw00.record@gmail.com>
2024-03-29 00:34:06 +0900

    최초 커밋

---
+++ src/main/java/com/example/jetsetgo_back/JetSetGoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.jetsetgo_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetSetGoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetSetGoBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jetsetgo_back/JetSetGoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jetsetgo_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetSetGoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
