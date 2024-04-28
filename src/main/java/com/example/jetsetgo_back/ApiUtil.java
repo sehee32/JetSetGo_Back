e8d9c618d4da25c703053ee90f42de9efa025b29
문유정 <adw00.record@gmail.com>
2024-04-28 17:32:04 +0900

    api 테스트 코드

---
+++ src/main/java/com/example/jetsetgo_back/ApiUtil.java
@@ -0,0 +1,51 @@
+package com.example.jetsetgo_back;
+
+import com.google.gson.Gson;
+import org.json.JSONObject;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.URL;
+import java.util.Map;
+
+public class ApiUtil {
+
+    private static String client_id = "SDDRMcSS3MEFhy94eLGkY0OTRpRL4UhD";
+    private static String client_secret = "My5F2fQLQcMqRAkc";
+
+
+    /* 토큰 조회 */
+    public static String getToken() throws IOException {
+
+        HttpsURLConnection conn = null;
+
+        URL url = new URL("https://test.api.amadeus.com/v1/security/oauth2/token");
+
+        conn = (HttpsURLConnection) url.openConnection();
+
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("body", "grant_type=client_credentials&client_id="+client_id+"&client_secret="+client_secret);
+        conn.setDoOutput(true);
+
+        JSONObject json = new JSONObject();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+
+        bw.write(json.toString());
+        bw.flush();
+        bw.close();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+
+        Gson gson = new Gson();
+
+        String response = gson.fromJson(br.readLine(), Map.class).get("response").toString();
+        String token = gson.fromJson(response, Map.class).get("access_token").toString();
+
+        br.close();
+        conn.disconnect();
+
+        return token;
+    }
+}
--- src/main/java/com/example/jetsetgo_back/HelloWord.java
+++ src/main/java/com/example/jetsetgo_back/HelloWord.java
@@ -4,12 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api")
 public class HelloWord {
 
     @GetMapping("/Hello")
-    public String helloWorld() {
-        return "hello!";
+    public String helloWorld() throws IOException {
+
+        return "aa";
+    }
+
+    @GetMapping("/Hello2")
+    public String helloWorld2() throws IOException {
+
+        return ApiUtil.getToken();
     }
 }
