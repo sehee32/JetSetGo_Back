7738d55e6731c77b2976fa4b7491840a46e4b4ed
문유정 <adw00.record@gmail.com>
2024-07-17 19:33:12 +0900

    오류 수정

--- src/main/java/kr/co/jetsetgo/controller/CommonController.java
+++ src/main/java/kr/co/jetsetgo/controller/CommonController.java
@@ -14,20 +14,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class CommonController {
 
-    @Autowired
-    private HelloWordService helloWordService;
 
-    /**
-     * Sample Controller
-     * 호출 예시 : http://localhost:8080/api/Hello?data=호출테스트
-     * 결과 : 호출테스트 Hello Word
-     *
-     * @param data
-     * @return
-     */
-    @GetMapping(value = "/Hello")
-    public String selectHelloWorld(@RequestParam String data) {
-
-        return helloWordService.selectHelloWorld(data);
-    }
 }
