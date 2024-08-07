01b9d5115cd4e9c400211970778793da06307899
문유정 <adw00.record@gmail.com>
2024-08-07 21:22:11 +0900

    mapper 수정

--- src/main/java/kr/co/jetsetgo/dbio/SignUpMapper.java
+++ src/main/java/kr/co/jetsetgo/dbio/SignUpMapper.java
@@ -1,16 +1,12 @@
 package kr.co.jetsetgo.dbio;
 
 import kr.co.jetsetgo.model.SignUpDto;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
 public interface SignUpMapper {
-    @Insert("INSERT INTO MEMBERS (NAME, USERNAME, PASSWORD, BIRTHDATE, PHONENUMBER, AGREETERMS) VALUES (#{name}, #{username}, #{password}, #{birthdate}, #{phoneNumber}, #{agreeTerms})")
     void insertMember(SignUpDto signUpDto);
 
-    @Select("SELECT * FROM MEMBERS WHERE USERNAME = #{username}")
     SignUpDto findByUsername(String username);
 }
